977ad6d34b08ac351f4e16efb31c1a8e9ea6772b
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-06-29 22:42:29 +0900

    2024.06.29 스프링 기초 공부
    Autowired, 동일한 타입의 빈이 여러 개일 때 -> primary, qualifier
    의존관계 주입은 생성자 주입 방법 사용하기

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,12 +6,20 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // @RequiredArgsConstructor 얘가 final붙은거로 생성자 만들어줌
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
@@ -23,5 +31,10 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,23 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+
+public class SingletonService {
+
+    //1. static 영역에 객체를 하나만 생성해둔다.
+    private static final SingletonService instance = new SingletonService();
+
+    //2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해 조회하도록 허용
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    //3. 생성자를 private으로 선언해서 new 키워드를 사용한 객체 생성을 막는다
+    private SingletonService() {
+
+    }
+
+    public void login(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
